cad32280e31a576fbe307a5a28a7aff047e804c5
Daniel Del Core <daniel.delcore.92@gmail.com>
2020-05-27 18:10:10 +1000

    🐛 Export useRenderCount

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { default as useStableRefTester } from './useStableRefTester';
 export { default as useWhichDepChanged } from './useWhichDepChanged';
 export { default as RenderCount, RenderCountProps } from './RenderCount';
-export { default as useRenderCount } from './RenderCount';
+export { default as useRenderCount } from './useRenderCount';
